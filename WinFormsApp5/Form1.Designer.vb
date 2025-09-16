2924391664f188807dfd0212d37fcf44032f8cba
User <User@SUACILLO-012205>
2025-09-16 23:32:25 +0800

    Add project files.

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ComboBox1 = New ComboBox()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        DataGridView1 = New DataGridView()
+        DELETE = New Button()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(12, 12)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(12, 46)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(94, 24)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "READING"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(12, 76)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(82, 24)
+        CheckBox2.TabIndex = 2
+        CheckBox2.Text = "SPORTS"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(12, 106)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(75, 24)
+        CheckBox3.TabIndex = 3
+        CheckBox3.Text = "MUSIC"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(185, 46)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(68, 24)
+        RadioButton1.TabIndex = 4
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "MALE"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(185, 76)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(83, 24)
+        RadioButton2.TabIndex = 5
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "FEMALE"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(501, 29)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(126, 97)
+        Button1.TabIndex = 6
+        Button1.Text = "SHOW RESULT"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(18, 217)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(64, 20)
+        Label1.TabIndex = 7
+        Label1.Text = "COURSE"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(18, 253)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(69, 20)
+        Label2.TabIndex = 8
+        Label2.Text = "HOBBIES"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(18, 290)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(66, 20)
+        Label3.TabIndex = 9
+        Label3.Text = "GENDER"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3})
+        DataGridView1.Location = New Point(316, 180)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.ReadOnly = True
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(447, 208)
+        DataGridView1.TabIndex = 10
+        ' 
+        ' DELETE
+        ' 
+        DELETE.Location = New Point(451, 394)
+        DELETE.Name = "DELETE"
+        DELETE.Size = New Size(176, 44)
+        DELETE.TabIndex = 11
+        DELETE.Text = "DELETE"
+        DELETE.UseVisualStyleBackColor = True
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "COURSE"
+        Column1.MinimumWidth = 6
+        Column1.Name = "Column1"
+        Column1.ReadOnly = True
+        Column1.Width = 125
+        ' 
+        ' Column2
+        ' 
+        Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        Column2.HeaderText = "HOBBIES"
+        Column2.MinimumWidth = 6
+        Column2.Name = "Column2"
+        Column2.ReadOnly = True
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "GENDER"
+        Column3.MinimumWidth = 6
+        Column3.Name = "Column3"
+        Column3.ReadOnly = True
+        Column3.Width = 125
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DELETE)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DELETE As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 🔹 Populate ComboBox with courses
+        ComboBox1.Items.AddRange({"BSIT", "BSHRM", "BSA", "BEED", "BSED", "BSBA", "BSMT", "BSCE"})
+
+        ' 🔹 Set default labels
+        Label1.Text = "Course: "
+        Label2.Text = "Hobbies: "
+        Label3.Text = "Gender: "
+
+        ' 🔹 Setup DataGridView columns (only once)
+        With DataGridView1
+            .Columns.Clear()
+            .Columns.Add("colCourse", "COURSE")
+            .Columns.Add("colHobbies", "HOBBIES")
+            .Columns.Add("colGender", "GENDER")
+            .AllowUserToAddRows = False
+            .ReadOnly = True
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' 🔹 Get selected course
+        Dim selectedCourse As String = If(ComboBox1.SelectedIndex <> -1, ComboBox1.SelectedItem.ToString(), "No course selected")
+
+        ' 🔹 Get selected hobbies
+        Dim hobbies As New List(Of String)
+        If CheckBox1.Checked Then hobbies.Add(CheckBox1.Text)
+        If CheckBox2.Checked Then hobbies.Add(CheckBox2.Text)
+        If CheckBox3.Checked Then hobbies.Add(CheckBox3.Text)
+        Dim selectedHobbies As String = If(hobbies.Count > 0, String.Join(", ", hobbies), "No hobbies selected")
+
+        ' 🔹 Get selected gender
+        Dim selectedGender As String
+        If RadioButton1.Checked Then
+            selectedGender = RadioButton1.Text
+        ElseIf RadioButton2.Checked Then
+            selectedGender = RadioButton2.Text
+        Else
+            selectedGender = "No gender selected"
+        End If
+
+        ' 🔹 Display results in Labels
+        Label1.Text = "Course: " & selectedCourse
+        Label2.Text = "Hobbies: " & selectedHobbies
+        Label3.Text = "Gender: " & selectedGender
+
+        ' 🔹 Insert results into DataGridView
+        DataGridView1.Rows.Add(selectedCourse, selectedHobbies, selectedGender)
+    End Sub
+
+    Private Sub DELETE_Click(sender As Object, e As EventArgs) Handles DELETE.Click
+        ' 🔹 Delete selected row
+        If DataGridView1.SelectedRows.Count > 0 Then
+            DataGridView1.Rows.RemoveAt(DataGridView1.SelectedRows(0).Index)
+        Else
+            MessageBox.Show("Please select a row to delete.", "Delete Row", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+End Class
